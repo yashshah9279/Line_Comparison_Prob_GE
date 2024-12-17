3963aa722dc4e94c89c75334fb9ffd1b2849479f
Yash <yashshah@gmail.com>
2024-12-17 12:13:46 +0530

    Updated Welcome

---
+++ LineComp.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class LineComp {
+    public static void main(String[] args) {
+        // USE CASE 1 - Welcome Message
+        System.out.println("Welcome to the Line Comparison Problem");
+    }
+}
+
+
+
+
